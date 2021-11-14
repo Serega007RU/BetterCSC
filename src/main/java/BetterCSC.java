7e48bd91795fa7bb96ce993f7a7bf601b13be312
Serega007 <ermaksochi@gmail.com>
2021-11-14 19:57:30 +0300

    Разделяем запятой число если число превышает тыс, млн и т.д.

--- src/main/java/BetterCSC.java
+++ src/main/java/BetterCSC.java
@@ -309,7 +309,7 @@ public class BetterCSC implements ModMain, Listener {
                     if (msg.contains("Баланс: ") || msg.contains("Вы успешно улучшили предмет")) {
                         chatReceive.setCancelled(true);
                         return;
-                    } else if (msg.contains("У вас недостаточно золота на балансе") || msg.contains("Этот предмет нельзя улучшить") || msg.contains("Вы не находитесь в игре") || msg.contains("вы не можете сейчас открыть меню апгрейда")) {
+                    } else if (msg.contains("У вас недостаточно золота на балансе") || msg.contains("Этот предмет нельзя улучшить") || msg.contains("Вы не находитесь в игре") || msg.contains("вы не можете сейчас открыть меню апгрейда") || msg.contains("Этот предмет улучшен до максимального уровня")) {
                         chatReceive.setCancelled(true);
                         enableUP = false;
                         if (taskUP != null) taskUP.shutdown();
@@ -334,6 +334,13 @@ public class BetterCSC implements ModMain, Listener {
                     if (lastMessage != null) {
                         api.chat().printChatMessage(lastMessage);
                     }
+                    String text = chatReceive.getText().getFormattedText();
+                    String[] list = msg.replaceAll("[^-?0-9]+", " ").trim().split(" ");
+                    for (String str : list) {
+                        long num = Long.parseLong(str);
+                        text = text.replaceAll(str, new DecimalFormat("#,###").format(num));
+                    }
+                    chatReceive.setText(stringToText(text));
                     lastMessage = chatReceive.getText();
 //                    task = api.threadManagement().newSingleThreadedScheduledExecutor();
 //                    task.schedule(() -> {
@@ -357,12 +364,28 @@ public class BetterCSC implements ModMain, Listener {
                     if (msg.contains(" - ")) {
                         String text = msg;
                         text = msg.substring(text.indexOf(" - ") + 3, text.length());
-                        allBets += Long.parseLong(text);
+                        long num = Long.parseLong(text);
+                        allBets += num;
+
+                        String unfText = chatReceive.getText().getFormattedText();
+                        unfText = unfText.replaceAll(String.valueOf(num), new DecimalFormat("#,###").format(num));
+                        chatReceive.setText(stringToText(unfText));
                     } else {
                         api.chat().printChatMessage(Text.of("Общая сумма ставок: ", TextFormatting.GOLD, new DecimalFormat("#,###").format(allBets), TextFormatting.GOLD));
                         countBets = false;
                         allBets = 0;
                     }
+                } else if (msg.contains(" сорвал куш и получил ") || msg.contains(" сорвала джекпот и получила ")) {
+                    String var;
+                    if (msg.contains(" сорвал куш и получил ")) {
+                        var = " сорвал куш и получил ";
+                    } else {
+                        var = " сорвала джекпот и получила ";
+                    }
+                    String text = chatReceive.getText().getFormattedText();
+                    long num = Long.parseLong(msg.substring(msg.indexOf(var) + var.length(), msg.indexOf(" золота")));
+                    text = text.replaceAll(String.valueOf(num), new DecimalFormat("#,###").format(num));
+                    chatReceive.setText(stringToText(text));
                 }
 
                 if (lastMessage != null) {
@@ -539,7 +562,7 @@ public class BetterCSC implements ModMain, Listener {
         WindowClick.BUS.unregisterAll(this);
     }
 
-    public int getColor(int percent) {
+    private int getColor(int percent) {
         int red = (int) ((percent > 50 ? 1 - 2 * (percent - 50) / 100.0 : 1.0) * 255);
         int green = (int) ((percent > 50 ? 1.0 : 2 * percent / 100.0) * 255);
 //      int blue = 0;
@@ -548,5 +571,49 @@ public class BetterCSC implements ModMain, Listener {
         int blue = 0/* & 0x000000FF*/; //Mask out anything not blue.
 
         return 0xFF000000 | red | green | blue; //0xFF000000 for 100% Alpha. Bitwise OR everything together.
+    }
+
+    private final Map<Character, TextFormatting> textFormattingValues = new HashMap<Character, TextFormatting>() {{
+        put('0', TextFormatting.BLACK);
+        put('1', TextFormatting.DARK_BLUE);
+        put('2', TextFormatting.DARK_GREEN);
+        put('3', TextFormatting.DARK_AQUA);
+        put('4', TextFormatting.DARK_RED);
+        put('5', TextFormatting.DARK_PURPLE);
+        put('6', TextFormatting.GOLD);
+        put('7', TextFormatting.GRAY);
+        put('8', TextFormatting.DARK_GRAY);
+        put('9', TextFormatting.BLUE);
+        put('a', TextFormatting.GREEN);
+        put('b', TextFormatting.AQUA);
+        put('c', TextFormatting.RED);
+        put('d', TextFormatting.LIGHT_PURPLE);
+        put('e', TextFormatting.YELLOW);
+        put('f', TextFormatting.WHITE);
+        put('k', TextFormatting.OBFUSCATED);
+        put('l', TextFormatting.BOLD);
+        put('m', TextFormatting.STRIKETHROUGH);
+        put('n', TextFormatting.UNDERLINE);
+        put('o', TextFormatting.ITALIC);
+        put('r', TextFormatting.RESET);
+    }};
+
+    private Text stringToText(String text) {
+        String[] list = text.replaceAll("§", "regex").split("regex");
+        Text formText = Text.of("");
+        for (String str : list) {
+            if (str.length() == 0) continue;
+            char ch = str.charAt(0);
+            str = str.substring(1);
+            TextFormatting textFormatting = textFormattingValues.get(ch);
+            if (textFormatting == null) {
+                formText.append(Text.of(str));
+            } else if (textFormatting == TextFormatting.OBFUSCATED || textFormatting == TextFormatting.BOLD || textFormatting == TextFormatting.STRIKETHROUGH || textFormatting == TextFormatting.UNDERLINE) {
+                formText.append(Text.of(textFormatting));
+            } else {
+                formText.append(Text.of(str, textFormatting));
+            }
+        }
+        return formText;
     }
 }
